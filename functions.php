05db7a0947f0a65fe3e862da6a0172369c7fab86
vijendramisra <iamonetoughguy@gmail.com>
2014-01-10 11:58:39 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -9,9 +9,9 @@ function register_my_menu() {
 }
 add_action( 'init', 'register_my_menu' );
 
-function add_menuclass($ulclass) {
-return preg_replace('/<a /', '<a class="nav_text"', $ulclass, 1);
+/*function add_menuclass($ulclass) {
+	return preg_replace('/<a /', '<a class="nav_text"', $ulclass, 1);
 }
-add_filter('wp_nav_menu','add_menuclass');
+add_filter('wp_nav_menu','add_menuclass');*/
 
 ?>
