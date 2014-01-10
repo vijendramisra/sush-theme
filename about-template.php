0c925cb6d63e58eb1f402980c01b1f4cd2efe099
vijendramisra <iamonetoughguy@gmail.com>
2014-01-10 07:36:54 +0530

    Header added in about template

--- about-template.php
+++ about-template.php
@@ -3,65 +3,8 @@
 Template Name: About Template
 */
 ?>
-<html>
-<head>
-	<title><?php wp_title(''); ?></title>
-	<!-- HEADER -->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
-	<link href="<?php bloginfo( 'template_url' ); ?>/css/bootstrap.css" rel="stylesheet">
+get_header();
 
-	<link rel="stylesheet" href="<?php bloginfo( 'template_url' ); ?>/css/font-awesome.css">
-
-	<!-- Custom CSS -->
-	
-	<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=PT+Sans+Caption:400,700' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700' rel='stylesheet' type='text/css'>
-	<!-- <link rel="stylesheet" href="css/animate.css">	 -->
-	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">	
-	<!-- <link rel="stylesheet" href="css/global.css">	 -->
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	<?php wp_head(); ?>
-</head>
-
-<body>
-	<!-- NAVIGATION HEADER -->
-		<!-- NAV MENU -->
-	<!-- ####################### -->	
-
-	<nav class="navbar navbar-inverse navbar-fixed-top custom_nav" role="navigation">
-		<div class="container">			
-	  <!-- Brand and toggle get grouped for better mobile display -->
-	  <div class="navbar-header">
-	    <button type="button" class="navbar-toggle custom_toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-	      <span class="sr-only">Toggle navigation</span>
-	      <span class="icon-bar"></span>
-	      <span class="icon-bar"></span>
-	      <span class="icon-bar"></span>
-	    </button>
-	    <a class="navbar-brand brand_logo" href="index.php">SUSHMOBILE</a>
-	  </div>
-
-	  <!-- Collect the nav links, forms, and other content for toggling -->
-	  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	    	    
-	    <ul class="nav navbar-nav navbar-right">
-	    	<li><a class="nav_text" href="apps.php">OUR APPS</a></li>
-	    	<li><a class="nav_text" href="solutions.php">HOW WE WORK</a></li>			
-	    	<li><a class="nav_text" href="clients.php">CLIENTS</a></li>
-	      	<li><a class="nav_text" href="blog.php">BLOG</a></li>
-	      	<li><a class="nav_text" href="about_us.php">ABOUT US</a></li>
-	      	<li class="btn-send hidden-xs">
-	      		<a class="nav_text" href="#footer">
-	      			<span class="icon_send glyphicon glyphicon-send"></span>
-	      		</a>
-	      	</li>
-	    </ul>
-	  </div><!-- /.navbar-collapse -->
-
-	  </div>
-	</nav>	
 
 	<!-- ABOUT SUBMENU -->
 	<div class="submenu_bg">
