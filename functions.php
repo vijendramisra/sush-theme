d96917fe826d8a4b446f0ce568086b3da7e588ce
vijendramisra <iamonetoughguy@gmail.com>
2014-01-10 10:53:48 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,11 @@
 <?php
 
 function register_my_menu() {
-  register_nav_menu('header-menu',__( 'Header Menu' ));
+  register_nav_menu(array(
+	'header-menu' => 'Header Menu',
+	'about-menu' => 'About Menu',
+    'work-menu' => 'Work Menu'
+));
 }
 add_action( 'init', 'register_my_menu' );
 
